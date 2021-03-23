79a31b5ce5696d3c8d9e5acb585a8d5a2e6731ff
chhunsokhon18 <chhunsokhon18@kit.edu.kh>
2021-03-23 22:04:43 +0700

    add vue router & ssr

---
+++ src/views/About.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  head: {
+    meta: [
+      {
+        name: "title",
+        title: "About",
+      },
+      {
+        property: "og:title",
+        content: "Facebook Title",
+        id: "ogtitle",
+      },
+      {
+        property: "og:image",
+        content: "Facebook.jpg",
+        id: "ogimg",
+      },
+      { property: "fb:app_id", content: "123456789", id: "ogfb" },
+      {
+        property: "og:description",
+        content:
+          "Facebook Description",
+        id: "ogdes",
+      },
+      {
+        name: "twitter:card",
+        content:
+          "summary_large_image",
+        id:"twcard",
+      },
+      {
+        name: "twitter:title",
+        content:"Twitter title",
+        id:"twtitle",
+      },
+      {
+        name: "twitter:image",
+        content:"Twitter image",
+        id:"twimage",
+      },  
+    ]
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  head: {
+    meta: [
+      {
+        name: "title",
+        title: "Home",
+      },
+      {
+        property: "og:title",
+        content: "Facebook Title",
+        id: "ogtitle",
+      },
+      {
+        property: "og:image",
+        content: "FacebookImage.jpg",
+        id: "ogimg",
+      },
+      { property: "fb:app_id", content: "123456789", id: "ogfb" },
+      {
+        property: "og:description",
+        content:
+          "Facebook description",
+        id: "ogdes",
+      },
+      {
+        name: "twitter:card",
+        content:
+          "summary_large_image",
+        id:"twcard",
+      },
+      {
+        name: "twitter:title",
+        content:"Twitter title",
+        id:"twtitle",
+      },
+      {
+        name: "twitter:image",
+        content:"TwitterImage.jpg",
+        id:"twimage",
+      },  
+    ],
+  }
+}
+</script>
